a2128776d1556c9dcb1aa7e4bc383edc8cd5c4f9
ramarks <marks.265@osu.edu>
2018-10-20 02:14:02 -0400

    adding real app

---
+++ refugeeMatch/src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MatchComponent } from './match/match.component';
+import { GeneralComponent } from './general/general.component';
+import { MentorComponent } from './mentor/mentor.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'match',
+    component: MatchComponent
+  },
+  {
+    path: 'general',
+    component: GeneralComponent
+  },
+  {
+    path: 'mentor',
+    component: MentorComponent
+  },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ refugeeMatch/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MatchComponent } from './match/match.component';
+import { MentorComponent } from './mentor/mentor.component';
+import { GeneralComponent } from './general/general.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    MatchComponent,
+    MentorComponent,
+    GeneralComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
