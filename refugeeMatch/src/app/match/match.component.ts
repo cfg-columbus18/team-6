8949edbb134255c51db74ef4f6b54db2c9f78528
ramarks <marks.265@osu.edu>
2018-10-20 07:22:48 -0400

    added chat repo

--- refugeeMatch/src/app/match/match.component.ts
+++ refugeeMatch/src/app/match/match.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MatchComponent implements OnInit {
 
+
+  show: boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
+  Show(): void {
+    this.show = !this.show;
+  }
+}
+export class Match{
 
+  country: string;
+  language: string;
+  time: string;
+  topic1: string;
+  topic2: string;
+  topic3: string;
+  other: string;
+
+  constructor(country,language,time,topic1, topic2, topic3){
+      this.country = country;
+      this.language = language;
+      this.time = time;
+      this.topic1 = topic1;
+      this.topic2 = topic2;
+      this.topic3 = topic3;
+
+  }
 }
